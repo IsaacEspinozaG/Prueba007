fe7b934f37ca88a7bfd307e6d038d89326824244
Isaac Espinoza G <l18330428@hermosillo.tecnm.mx>
2022-10-01 19:53:12 -0700

    la esto es una prueba

---
+++ Pro Esthetic/Pro Esthetic/Models/Usuario.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Pro_Esthetic.Models
+{
+    public partial class Usuario
+    {
+        public int Id { get; set; }
+        public string? Nombre { get; set; }
+        public string? Telefono { get; set; }
+    }
+}
